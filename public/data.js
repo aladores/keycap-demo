c37059ae8f11e83141f6659d9aa84d1bf3cff90b
aladores <angelo.ladores@outlook.com>
2023-09-05 20:51:55 -0400

    add layout and local data

---
+++ public/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        id: "1",
+        attributes: {
+            title: "GMK Noire",
+            imageSrc: "https://i.imgur.com/r1eWSH3.jpg",
+            description: "Inspired by a need for a set that would work well on as many boards as possible, regardless of design, layout, budget or color; and inspired by midcentury modern and minimalist design, GMK Noire is currently live on the following vendor sites",
+        },
+    },
+    {
+        id: "2",
+        attributes: {
+            title: "GMK Masterpiece",
+            imageSrc: "https://www.deskhero.ca/cdn/shop/products/Gvk0DMx_b1b248ca-26ad-4139-90e8-70fdc4edd77f_628x628.jpg?v=1649797041",
+            description: "Limited time opportunity to purchase a truly aesthetic, clean, unique and versatile keyset with 85 new molds for only $129 USD. This is by far the most unique GMK set that made it into Group Buy phase the traditional way, from ordinary Interest Check to Group Buy. Impress your friends and colleagues with this high quality keyset!",
+        },
+    },
+    {
+        id: "3",
+        attributes: {
+            title: "Gmk Metropolis",
+            imageSrc: "https://oblotzky.industries/cdn/shop/products/gmk_metropolis_2_keyboard_no2rev1wk_persp_right_close_590x.jpg?v=1617285996",
+            description: "Return to Metropolis with the highly anticipated second run of this iconic keyset. Featuring additional icon novelties, re-imagined kits, extended coverage, and some excellent collaborations from our friends at RAMA WORKS.",
+        },
+    },
+    {
+        id: "4",
+        attributes: {
+            title: "GMK Cafe",
+            imageSrc: "https://www.deskhero.ca/cdn/shop/products/IuPTzi6_a70f4ff0-d3eb-42b5-8916-7fccf9540a8e_628x468.jpg?v=1574106992",
+            description: "As coffee Café gained popularity in Italy in the 16th century, clergymen pushed the church to ban this new, scary drink as \"satanic.\" This discussion was brought all the way to the Pope himself: Clement VIII. The literal freakin' Pope took one sip and found this beverage so wonderful he remarked how they should \"baptize it and keep it for themselves.\" Café is incredible enough to change the mind of a pope and it's darn sure wonderful enough to put on your keyboard, too.",
+        },
+    },
+];

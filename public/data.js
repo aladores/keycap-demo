17f267fb7189e5ba468e257cac5a8d1de3b0ae4f
aladores <angelo.ladores@outlook.com>
2023-09-06 02:55:28 -0400

    add kits and ref link

--- public/data.js
+++ public/data.js
@@ -1,14 +1,19 @@
 export const data = [
     {
         id: "1",
+        featured: true,
         attributes: {
             title: "GMK Noire",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/3NCBRQM_925ad50c-9782-423a-9365-9eeb4cd4cdb8_626x418.png?v=1652461455",
-            description: "Inspired by a need for a set that would work well on as many boards as possible, regardless of design, layout, budget or color; and inspired by midcentury modern and minimalist design, GMK Noire is currently live on the following vendor sites",
+            description: "Inspired by a need for a set that would work well on as many boards as possible, regardless of design, layout, budget or color; and inspired by midcentury modern and minimalist design",
+            groupBuyLink: "https://geekhack.org/index.php?topic=108890.0",
+            price: "",
+            kits: ["Base", "Novelties"],
         },
     },
     {
         id: "2",
+        featured: true,
         attributes: {
             title: "GMK Masterpiece",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/Value-Base_b9fae8c8-b5e0-4776-a81d-11454697cb3d_626x331.png?v=1649797041",
@@ -17,14 +22,16 @@ export const data = [
     },
     {
         id: "3",
+        featured: true,
         attributes: {
-            title: "Gmk Metropolis",
+            title: "GMK Metropolis",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/gmk_metropolis_r2_kit_01_base_a64756f4-f3b0-4d8d-b537-41b22056cdbf_760x507.png?v=1675889653",
             description: "Return to Metropolis with the highly anticipated second run of this iconic keyset. Featuring additional icon novelties, re-imagined kits, extended coverage, and some excellent collaborations from our friends at RAMA WORKS.",
         },
     },
     {
         id: "4",
+        featured: true,
         attributes: {
             title: "GMK Cafe",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/hkIXqAQ_2142df1a-ec84-413f-80cc-fd390a831eb1_626x469.png?v=1574106992",

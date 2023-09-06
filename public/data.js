9e89c77f19da11ac4ed02ae02c2781680f3e19bf
aladores <angelo.ladores@outlook.com>
2023-09-06 04:34:55 -0400

    add more info and pricing conditional

--- public/data.js
+++ public/data.js
@@ -6,10 +6,11 @@ export const data = [
             title: "GMK Noire",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/3NCBRQM_925ad50c-9782-423a-9365-9eeb4cd4cdb8_626x418.png?v=1652461455",
             description: "Inspired by a need for a set that would work well on as many boards as possible, regardless of design, layout, budget or color; and inspired by midcentury modern and minimalist design",
+            designer: "apiary",
             groupBuyLink: "https://geekhack.org/index.php?topic=108890.0",
             kits: [
-                { name: "Base", price: { USD: 99.99, CAD: 129.99 } },
-                { name: "Novelties", price: { USD: 49.99, CAD: 64.99 } }
+                { name: "Base", price: { USD: 140.00, EUR: 129.00, CAD: 190.00 } },
+                { name: "Novelties", price: { USD: 45.00, EUR: 39.00, CAD: 62.00 } }
             ]
         },
     },
@@ -19,7 +20,16 @@ export const data = [
         attributes: {
             title: "GMK Masterpiece",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/Value-Base_b9fae8c8-b5e0-4776-a81d-11454697cb3d_626x331.png?v=1649797041",
-            description: "Limited time opportunity to purchase a truly aesthetic, clean, unique and versatile keyset with 85 new molds for only $129 USD. This is by far the most unique GMK set that made it into Group Buy phase the traditional way, from ordinary Interest Check to Group Buy. Impress your friends and colleagues with this high quality keyset!",
+            description: "The inspiration for this set comes from the Japanese high quality craftsmanship. Japanese craftsmanship is handmade and created by people using skills passed down for generations to ensure only the best quality product - a true masterpiece. With that spirit in mind I designed a keycap set, capturing the unique aesthetics of the Japanese culture. The Katakana legends will give the set a very unique look, paired with a beautiful rich blue.",
+            designer: "Energieschleuder",
+            groupBuyLink: "https://geekhack.org/index.php?topic=106068.0",
+            kits: [
+                { name: "Base", price: { USD: 140.00, EUR: 139.00, AUD: 230.00, SGD: 198.00, CAD: 190.00 } },
+                { name: "Clarity", price: { USD: 45.00, EUR: 56.00, AUD: 95.00, SGD: 75.00, CAD: 73.00 } },
+                { name: "Preserving", price: { USD: 45.00, EUR: 38.00, AUD: 60.00, SGD: 54.00, CAD: 52.00 } },
+                { name: "West", price: { USD: 45.00, EUR: 66.00, AUD: 100.00, SGD: 91.00, CAD: 88.00 } }
+
+            ]
         },
     },
     {
@@ -29,6 +39,17 @@ export const data = [
             title: "GMK Metropolis",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/gmk_metropolis_r2_kit_01_base_a64756f4-f3b0-4d8d-b537-41b22056cdbf_760x507.png?v=1675889653",
             description: "Return to Metropolis with the highly anticipated second run of this iconic keyset. Featuring additional icon novelties, re-imagined kits, extended coverage, and some excellent collaborations from our friends at RAMA WORKS.",
+            designer: "Nephlock",
+            groupBuyLink: "https://geekhack.org/index.php?topic=108890.0",
+            kits: [
+                { name: "Base", price: {} },
+                { name: "Midnight", price: {} },
+                { name: "Signs", price: {} },
+                { name: "Turnpike", price: {} },
+                { name: "Spacebars", price: {} },
+                { name: "Compact", price: {} }
+
+            ]
         },
     },
     {
@@ -38,6 +59,16 @@ export const data = [
             title: "GMK Cafe",
             imageSrc: "https://www.deskhero.ca/cdn/shop/products/hkIXqAQ_2142df1a-ec84-413f-80cc-fd390a831eb1_626x469.png?v=1574106992",
             description: "As coffee Café gained popularity in Italy in the 16th century, clergymen pushed the church to ban this new, scary drink as \"satanic.\" This discussion was brought all the way to the Pope himself: Clement VIII. The literal freakin' Pope took one sip and found this beverage so wonderful he remarked how they should \"baptize it and keep it for themselves.\" Café is incredible enough to change the mind of a pope and it's darn sure wonderful enough to put on your keyboard, too.",
+            designer: "Langelandia",
+            groupBuyLink: "https://geekhack.org/index.php?topic=102580.0",
+            kits: [
+                { name: "Base", price: { USD: 125.00, EUR: 129.00, AUD: 200.00, SGD: 185.00, CAD: 175.00 } },
+                { name: "Numpad/ISO", price: { USD: 48.00, EUR: 50.00, AUD: 76.00, SGD: 71.00, CAD: 67.00 } },
+                { name: "Dark - Alphas", price: { USD: 66.00, EUR: 67.00, AUD: 105.00, SGD: 98.00, CAD: 95.00 } },
+                { name: "Spacebars", price: { USD: 23.00, EUR: 22.00, AUD: 36.00, SGD: 34.00, CAD: 33.00 } },
+                { name: "40s/ Ortho", price: { USD: 52.00, EUR: 49.00, AUD: 83.00, SGD: 77.00, CAD: 75.00 } },
+
+            ]
         },
     },
 ];

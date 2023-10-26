1a25d40fd1f528f784980a3f45e94a685c10fecc
aladores <angelo.ladores@outlook.com>
2023-10-25 21:24:47 -0400

    remove github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://a.ladores.github.io',
-  base: '/keycap-demo',
   integrations: [preact()]
 });
